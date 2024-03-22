51ee145855df480ef888e32272c1f4c9525555f8
Chang Liao <20618384+changliao1025@users.noreply.github.com>
2024-03-22 13:09:48 -0700

    add more options

--- examples/mosart/mosart_amazon_example.py
+++ examples/mosart/mosart_amazon_example.py
@@ -5,21 +5,39 @@ import netCDF4 as nc
 
 #you only need change the path to the package if if it is not installed in your python environment, 
 #this is for testing purpose
-sPath_geovista = '/qfs/people/liao313/workspace/python/geovista/src'
-#sPath_geovista = '/Users/liao313/workspace/python/geovista/src'
+sPath = str( Path().resolve(2) )
+print(sPath)
+iFlag_platform = 3
+iFlag_monthly = 1
 sVariable = 'FLOODED_FRACTION'
+sCase = 'e3sm20240102001'
+
+if iFlag_platform == 1:
+    sPath_geovista = '/qfs/people/liao313/workspace/python/geovista/src' #linux hpc
+    sWorkspace_data = realpath( sPath +  '/data/mosart/amazon' )
+    sWorkspace_data_aux = sWorkspace_data
+else:
+    if iFlag_platform ==2:
+       sPath_geovista = '/Users/liao313/workspace/python/geovista/src' #mac
+       sWorkspace_data = realpath( sPath +  '/data/mosart/amazon' )
+       sWorkspace_data_aux = sWorkspace_data
+    else:
+       sPath_geovista = 'C:\workspace\python\geovista\src'  #windows, we may also use mapped driver as data source
+       if iFlag_monthly ==1:
+           sWorkspace_data = realpath( sPath +  '/data/mosart/amazon' )
+           sWorkspace_data_aux = sWorkspace_data
+       else:
+           sWorkspace_data = r"Z:\e3sm_scratch\amazon\\" + sCase  + r"\run"
+           sWorkspace_data_aux = r"Z:\04model\e3sm\amazon\cases_aux\\" + sCase  
 
 sys.path.append(sPath_geovista)
 
 import geovista as gv
 import geovista.theme
 
-sPath = str( Path().resolve(2) )
-print(sPath)
-sWorkspace_data = realpath( sPath +  '/data/mosart/amazon' )
 # Load the sample data.
 
-sFilename_domain = sWorkspace_data + '/mosart_amazon_domain.nc'
+sFilename_domain = sWorkspace_data_aux + '/mosart_amazon_domain.nc'
 sFilename_timeseries = sWorkspace_data + '/e3sm20240102001.mosart.h0.2019-01.nc'
 
 dataset = nc.Dataset(sFilename_domain)
